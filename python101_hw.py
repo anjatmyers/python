7ff7d0195797f3c27c6df62311a8bd457cbe686d
Andrea Myers <andreatmyers5@gmail.com>
2020-11-11 20:36:02 -0500

    Wed. homework problems

---
+++ python101_hw.py
@@ -0,0 +1,90 @@
+# # Tip calculator with split the bill function
+
+# bill = float(input("How much was your bill?"))
+# rating = input("Would you rate your service as: good, fair, or bad?").strip().lower()
+# tip = 0
+# people = int(input("How many people are splitting the bill?"))
+# if rating == "good":
+#     tip = 0.20
+# elif rating == "fair":
+#     tip = 0.15
+# elif rating == "bad":
+#     tip = 0.10
+# else:
+#     print("Please rate your service as good, fair or bad.")
+#     rating = input("Would you rate your service as: good, fair, or bad?") 
+
+# total_amount = (bill + (bill * tip))
+# div_bill = total_amount/people
+# print(f"Your total bill is ${total_amount:.2f}")
+# print(f"Each person will pay ${div_bill:.2f}")
+
+
+
+
+# Coin counter 
+# counter = 0 
+# answer = input("Do you want a coin?").strip().lower()
+# while answer == "yes":
+#     counter +=1 
+#     print(f"You have {counter} coins.")
+#     answer = input("Would you like another?").strip().lower()
+#     if answer == "no":
+#         print("Okay, then. Give me my money back.")
+
+
+
+
+
+
+# # Lets build a box 
+# print("Let's build a box!")
+# height = int(input("How many spaces tall should it be?"))
+# width = int(input("How many spaces wide should it be?"))
+
+# box_top = ("*" * width)
+# print(box_top)
+
+# for i in range(height - 2):
+#     print("*" + " " * (width - 2) + "*")
+
+# box_top = ("*" * width)
+# print(box_top)
+##try joe's suggestion of using range for row and height (for row = 0 and height -1)
+
+
+# print a triangle
+
+# print("Let's build a triangle!")
+# num_of_rows = int(input("How many rows tall should the triangle be?"))
+# spaces = num_of_rows
+# stars = -1
+
+# for i in range(num_of_rows):
+#     spaces -= 1
+#     stars += 2 
+#     print(" " * spaces + "*" * stars)
+
+
+#multiplication table 
+
+# first_int = 1
+# second_int = 1
+# for j in range(10):
+#     for i in range(10): 
+#         answer = first_int * second_int
+#         print(f"{first_int} X {second_int} = {answer}")
+#         second_int += 1
+#     second_int = second_int - 10 
+#     first_int += 1
+
+
+
+
+    
+
+
+
+
+
+
